99681c50a75fbf3789c1edbbbcc507c563cb8cf7
Han <hwl784770@outlook.com>
2022-03-29 21:40:38 +0800

    add Note in Test02 on concurrency4 package

--- src/main/java/com/han/concurrency4/Test02.java
+++ src/main/java/com/han/concurrency4/Test02.java
@@ -10,6 +10,17 @@ import java.util.stream.IntStream;
  * @author Han
  * @date 2022年03月29日 16:24
  */
+
+/**
+ * 传统上，我们可以通过synchronized关键字 + wait + notify/notifyAll 来实现多个线程之间的协调与通信，整个过程都是由JVM来帮助
+ * 我们实现的；开发者无需（也是无法）了解底层的实现细节
+ * <p>
+ * 从JDK 5开始，并发包提供了Lock， Condition(await与signal/signalAll)来实现多个线程之间的协调与通信，整个过程都是由开发者来
+ * 控制的，而且相比于传统方式，更加灵活，功能也更加强大
+ * <p>
+ * Thread.sleep与await（或是Object的wait方法）的本质区别：sleep方法本质上不会释放锁，而await会释放锁，并且在signal后，还需要
+ * 重新获得锁才能继续执行（该行为与Object的wait方法完全一致）
+ */
 public class Test02 {
     public static void main(String[] args) {
 
