70e1c4fe34458946a283938141122792af2643db
Han <hwl784770@outlook.com>
2022-03-31 00:57:14 +0800

    add Test03,Test04 in concurrency4 package

--- src/main/java/com/han/concurrency4/Test03.java
+++ src/main/java/com/han/concurrency4/Test03.java
@@ -30,23 +30,19 @@ package com.han.concurrency4;
  * 另外的重要一点在于：使用锁可能会导致线程的上下文切换（内核态与用户态之间的切换），但使用volatile并不会出现这种情况
  * <p>
  * ================ 这些就是错误示范，这样使用不能保证原子性 ===================
- * <p>
+ *
  * volatile int a = b + 2;
- * <p>
  * volatile int a = a++;
- * <p>
+ * volatile Date date = new Date();
+ *
  * ================ 这些就是正确示范 ===================
+ *
  * volatile int count = 1;
  * volatile boolean flag = false;
- * <p>
+ *
  * 如果要实现volatile写操作的原子性，那么在等号右侧的赋值变量中就不能出现被多线程所共享的变量，哪怕这个变量也是个volatile也不可以。
  * <p>
- * volatile Date date = new Date();
- * <p>
- * <p>
  * 防止指令重排序与实现变量的可见性都是通过一种手段来实现的：内存屏障（memory barrier）
- * <p>
- * <p>
  * <p>
  * int a = 1;
  * String s = "hello";
